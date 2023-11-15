942bb756160fbf542309e1639ce461d3ebe82667
Thayná de Castro Medeiros <182220014@senacinformatica.br>
2023-11-14 21:27:33 -0300

    Projeto iniciado

---
+++ src/controller/FarmaciaMvc.java
@@ -0,0 +1,18 @@
+
+package controller;
+
+import controller.view.Interface;
+import javax.swing.JFrame;
+
+public class FarmaciaMvc {
+
+    
+    public static void main(String[] args) {
+       
+        Interface inter = new Interface(); 
+        inter.setSize(805, 422);
+        inter.setVisible(true);
+        inter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
---
+++ src/controller/view/Interface.java
@@ -0,0 +1,108 @@
+
+package controller.view;
+
+import javax.swing.JFrame;
+
+
+public class Interface extends javax.swing.JFrame {
+
+   
+    public Interface() {
+        initComponents();
+    }
+
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        cadastraCliente = new javax.swing.JButton();
+        cadastraProduto = new javax.swing.JButton();
+        registraPedido = new javax.swing.JButton();
+        buscar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        cadastraCliente.setText("Cadastrar Cliente");
+        cadastraCliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cadastraClienteActionPerformed(evt);
+            }
+        });
+
+        cadastraProduto.setText("Cadastrar Produto");
+        cadastraProduto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cadastraProdutoActionPerformed(evt);
+            }
+        });
+
+        registraPedido.setText("Registrar Pedido");
+
+        buscar.setText("Buscar");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(49, 49, 49)
+                .addComponent(cadastraCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(54, 54, 54)
+                .addComponent(cadastraProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(64, 64, 64)
+                .addComponent(registraPedido, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 62, Short.MAX_VALUE)
+                .addComponent(buscar, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(47, 47, 47))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(312, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cadastraCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cadastraProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(registraPedido, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(buscar, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(72, 72, 72))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void cadastraClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cadastraClienteActionPerformed
+
+        CadastroCliente cadcli = new CadastroCliente(); 
+        cadcli.setSize(932, 449);
+        cadcli.setVisible(true);
+        cadcli.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }//GEN-LAST:event_cadastraClienteActionPerformed
+
+    private void cadastraProdutoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cadastraProdutoActionPerformed
+       
+        CadastroProduto cadprod = new CadastroProduto(); 
+        cadprod.setSize(932, 449);
+        cadprod.setVisible(true);
+        cadprod.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }//GEN-LAST:event_cadastraProdutoActionPerformed
+
+  
+    public static void main(String args[]) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Interface().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton buscar;
+    private javax.swing.JButton cadastraCliente;
+    private javax.swing.JButton cadastraProduto;
+    private javax.swing.JButton registraPedido;
+    // End of variables declaration//GEN-END:variables
+}
